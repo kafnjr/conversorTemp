8a67730dcaec1ca0a5af8f1bd9898f5aadff6892
kafnjr <cajunior.ps@outlook.com>
2021-11-02 23:51:27 -0300

    Programa finalizado

---
+++ conversorTemp/conversortemp/main.py
@@ -0,0 +1,11 @@
+import conversor
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Principal = QtWidgets.QMainWindow()
+    ui = conversor.Ui_conversor()
+    ui.setupUi(Principal)
+    Principal.show()
+    sys.exit(app.exec_())
